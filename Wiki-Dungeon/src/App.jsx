596a4aa5843a42a192d9abd2af684bbda2a2a553
Jason Ruan <jason.ruan@gwmail.gwu.edu>
2025-02-22 15:06:41 -0500

    2

--- Wiki-Dungeon/src/App.jsx
+++ Wiki-Dungeon/src/App.jsx
@@ -1,50 +1,15 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
-
-import React from "react";
-import Game from "./Game";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Game from "./components/Game";
+import { Title } from "./Title";
 function App() {
   return (
-    <div>
-      <h2>Wikipedia Dungeon</h2>
-      <Game />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<TitlePage />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
